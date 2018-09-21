6a53721205d476959196f58c5d8a1318c9f47cd2
Jordan Hurt <jth0024@gmail.com>
2018-09-20 22:39:22 -0400

    Support external get and set handlers
    Support external get and set handlers for things like HttpContext

--- src/__tests__/server/initialize.test.js
+++ src/__tests__/server/initialize.test.js
@@ -4,6 +4,10 @@
 const constants = require('../../utils/constants');
 const initialize = require('../../initialize');
 
+afterEach(() => {
+  delete global[constants.globalKey];
+});
+
 describe('initialize on server', () => {
   it('should assign the config to global with an initial state', () => {
     initialize({});
@@ -12,5 +16,26 @@ describe('initialize on server', () => {
         isFrozen: false,
       },
     });
+  });
+
+  it('should support custom handlers', () => {
+    const customStore = {};
+    const customImplementation = {
+      get(key) {
+        return customStore[key];
+      },
+      set(key, value) {
+        customStore[key] = value;
+      },
+    };
+    const initialState = { foo: 'bar' };
+    initialize(initialState, customImplementation);
+    expect(customStore[constants.globalKey]).toEqual({
+      [constants.stateKey]: {
+        isFrozen: false,
+      },
+      foo: 'bar',
+    });
+    expect(global[constants.globalKey]).toBe(undefined);
   });
 });
--- src/utils/environment.js
+++ src/utils/environment.js
@@ -3,7 +3,27 @@ const has = require('./has');
 module.exports = {
   getEnvironment,
   getInGlobal,
+  setHandlers,
   setInGlobal,
+};
+
+const implementation = {
+  get(key) {
+    const { isBrowser, isServer } = getEnvironment();
+
+    if (isBrowser && has(window, key)) return window[key];
+
+    if (isServer && has(global, key)) return global[key];
+
+    return undefined;
+  },
+  set(key, value) {
+    const { isBrowser, isServer } = getEnvironment();
+
+    if (isBrowser) window[key] = value;
+
+    if (isServer) global[key] = value;
+  },
 };
 
 function getEnvironment() {
@@ -17,19 +37,14 @@ function getEnvironment() {
 }
 
 function getInGlobal(key) {
-  const { isBrowser, isServer } = getEnvironment();
+  return implementation.get(key);
+}
 
-  if (isBrowser && has(window, key)) return window[key];
-
-  if (isServer && has(global, key)) return global[key];
-
-  return undefined;
+function setHandlers({ get, set }) {
+  if (get) implementation.get = get;
+  if (set) implementation.set = set;
 }
 
 function setInGlobal(key, value) {
-  const { isBrowser, isServer } = getEnvironment();
-
-  if (isBrowser) window[key] = value;
-
-  if (isServer) global[key] = value;
+  implementation.set(key, value);
 }
